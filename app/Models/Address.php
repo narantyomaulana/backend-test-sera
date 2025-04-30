1bc72b94647c16ea257d0e7daf90e924c7008b59
Narantyo Maulana <olandeoxys@gmail.com>
2025-04-30 10:53:18 +0700

    [ADD] Models Address and Make Schema Database

---
+++ app/Models/Address.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'street',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'is_primary',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
